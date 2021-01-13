1e8e83a95b7e22a6c0b7f363783eb485cc779cbd
Max Miller <maxmiller413@gmail.com>
2021-01-13 22:40:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,6 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status = 
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
