2c15f9afe209cc554ccc6546745931d52c7fa55a
Max Miller <maxmiller413@gmail.com>
2021-01-13 22:45:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,15 +1,20 @@
 def mario
-  puts phrase = "It's-a me, Mario!"
+  phrase = "It's-a me, Mario!"
+  puts phrase 
 end
 
 def toadstool
-  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts status
 end 
 
 def link 
-  puts 
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase 
 end 
 
 def all_phrases
-
+  mario
+  toadstool
+  link 
 end 
